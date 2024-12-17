0f2fecb538ac0556f1be625cf87051f183852fe9
Mizarc <contact@mizarc.dev>
2024-12-18 02:21:28 +1000

    Add script to generate immich metadata tag with game name
    This extracts the name of the game from the file name, makes it
    lowercase, then replaces the variable in the template. The resulting
    file can be read by immich, and contains the name of the game used as a
    tag.

---
+++ screenshots/GenerateMetadata.ps1
@@ -0,0 +1,40 @@
+﻿$folderPath = "Output"
+
+# Define the content to be written into each new file
+$contentTemplate = @"
+<?xpacket begin='﻿' id='W5M0MpCehiHzreSzNTczkc9d'?>
+<x:xmpmeta xmlns:x='adobe:ns:meta/' x:xmptk='Image::ExifTool 12.99'>
+<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
+
+ <rdf:Description rdf:about=''
+  xmlns:digiKam='http://www.digikam.org/ns/1.0/'>
+  <digiKam:TagsList>
+   <rdf:Seq>
+    <rdf:li>game/{GameName}</rdf:li>
+   </rdf:Seq>
+  </digiKam:TagsList>
+ </rdf:Description>
+</rdf:RDF>
+</x:xmpmeta>
+<?xpacket end='w'?>
+"@
+
+# Get all PNG files in the folder
+$imageFiles = Get-ChildItem -Path $folderPath -Filter *.png
+
+# Iterate through each PNG file
+foreach ($file in $imageFiles) {
+    # Replace the game name in the content with the game name in the file name
+    $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+    $gameName = ($fileNameWithoutExtension -replace 'Screenshot \d{4}-\d{2}-\d{2} \d{2}-\d{2}-\d{2} ', '').ToLower()
+    $content = $contentTemplate -replace '{GameName}', $gameName
+
+    # Define the new file name and path
+    $newFileName = "$($file.FullName).xmp"
+    $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+    
+    # Write the content to the new file
+    $content | Out-File -FilePath $newFileName -Encoding UTF8
+}
+
+Write-Host "Metadata creation completed!"

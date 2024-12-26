b6161e4b1b6ddbf10fbbe23dc8af67c22fb15eae
Mizarc <contact@mizarc.dev>
2024-12-26 18:03:31 +1000

    Change update modified date for screen rec for video files

--- screen recs/UpdateModifiedDate.ps1
+++ screen recs/UpdateModifiedDate.ps1
@@ -7,10 +7,10 @@ if (!$folder) {
 }
 
 # Get all PNG files in the folder
-$imageFiles = Get-ChildItem -Path $folder -Filter *.png
+$videoFiles = Get-ChildItem -Path $folder -Filter *.mkv
 
 # Iterate through each PNG file
-foreach ($file in $imageFiles) {
+foreach ($file in $videoFiles) {
     if ($file.Name -match 'Screen Rec (\d{4})-(\d{2})-(\d{2}) (\d{2})-(\d{2})-(\d{2})') {
         $newDate = Get-Date -Year $matches[1] -Month $matches[2] -Day $matches[3] -Hour $matches[4] -Minute $matches[5] -Second $matches[6]
         $file.LastWriteTime = $newDate

bf6a90bc3911e564da858957e8c046306f7bf855
Mizarc <contact@mizarc.dev>
2024-12-29 02:43:14 +1000

    Add timezone gathering and fetching on exif metadata

--- screen recs/UpdateExif.ps1
+++ screen recs/UpdateExif.ps1
@@ -2,6 +2,20 @@ param (
     [string]$folder
 )
 
+function Get-TimeZoneFromFileName {
+    param (
+        [string]$fileName
+    )
+
+    if ($fileName -match '\+(\d{2})(\d{2})') {
+        $hours = $matches[1] 
+        $minutes = $matches[2]
+        return "+${hours}:${minutes}"
+    } else { 
+        return ""
+    }
+}
+
 if (!$folder) {
   $folder = Read-Host "Please enter the folder path"
 }
@@ -12,9 +26,14 @@ $videoFiles = Get-ChildItem -Path $folder -Recurse -Include *.mp4, *.mkv -File
 # Iterate through each PNG file
 foreach ($file in $videoFiles) {
     if ($file.Name -match 'Screen Rec (\d{4})-(\d{2})-(\d{2}) (\d{2})-(\d{2})-(\d{2})') {
+        $timeZone = Get-TimeZoneFromFileName $file.Name
+        if ($timeZone -eq "") { 
+            $timeZone = "+00:00"
+        }
+
         $newDate = Get-Date -Year $matches[1] -Month $matches[2] -Day $matches[3] -Hour $matches[4] -Minute $matches[5] -Second $matches[6]
         $newDateString = $newDate.ToString("yyyy-MM-dd HH:mm:ss")
-        exifTool "-DateTimeOriginal=$newDateString+10:00" -overwrite_original $file.FullName
+        exifTool "-DateTimeOriginal=$newDateString$timeZone" -overwrite_original $file.FullName
     }
 }
 

bce9c91b72705af9be464f25d11548bcfc7deab7
Mizarc <contact@mizarc.dev>
2024-12-28 20:23:28 +1000

    Update crop scripts to refer to pre and post crop folders
    This change is necessary since the cropping process creates a new file,
    requiring the distinction of the two folders for ease.

--- screen recs/_crop_part1.bat
+++ screen recs/_crop_part1.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "Rename.ps1" -folder "Crop"
-powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateModifiedDate.ps1" -folder "Crop"
+powershell -NoProfile -ExecutionPolicy Bypass -File "Rename.ps1" -folder "Pre Crop"
+powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateModifiedDate.ps1" -folder "Pre Crop"
 pause
--- screen recs/_crop_part2.bat
+++ screen recs/_crop_part2.bat
@@ -1,15 +1,16 @@
 @echo off
 mkdir Output
-powershell -NoProfile -ExecutionPolicy Bypass -File "RenameFromModifiedDate.ps1" -folder "Crop"
-powershell -NoProfile -ExecutionPolicy Bypass -File "Process.ps1" -InputFolder "Crop" -OutputFolder "Output"
+powershell -NoProfile -ExecutionPolicy Bypass -File "RenameFromModifiedDate.ps1" -folder "Post Crop"
+powershell -NoProfile -ExecutionPolicy Bypass -File "Process.ps1" -InputFolder "Post Crop" -OutputFolder "Output"
 powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateModifiedDate.ps1" -folder "Output"
 powershell -NoProfile -ExecutionPolicy Bypass -File "GenerateMetadata.ps1" -folder "Output"
-powershell -NoProfile -ExecutionPolicy Bypass -File "Transfer.ps1" -InputFolder "Output" -OutputFolder "X:\game-clips"
+
+
 
 echo.
 set /p input=Clean up? (y/n): 
 if /i "%input%"=="y" (
-    powershell -NoProfile -ExecutionPolicy Bypass -File "Clear.ps1" -folder "Crop"
+    powershell -NoProfile -ExecutionPolicy Bypass -File "Clear.ps1" -folder "Post Crop"
 )
 
 echo Completed!

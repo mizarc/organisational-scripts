ce593da500b9dbc875df9e715ae032bac0bf9072
Mizarc <contact@mizarc.dev>
2024-12-29 21:45:19 +1000

    Add transfer script back to screenshot run script

--- screenshots/_run.bat
+++ screenshots/_run.bat
@@ -4,7 +4,7 @@ powershell -NoProfile -ExecutionPolicy Bypass -File "Rename.ps1" -folder "Input"
 powershell -NoProfile -ExecutionPolicy Bypass -File "Process.ps1" -InputFolder "Input" -OutputFolder "Output"
 powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateExif.ps1" -folder "Output"
 powershell -NoProfile -ExecutionPolicy Bypass -File "GenerateMetadata.ps1" -folder "Output"
-
+powershell -NoProfile -ExecutionPolicy Bypass -File "Transfer.ps1" -InputFolder "Output" -OutputFolder "X:\screenshots"
 
 echo.
 set /p input=Clean up? (y/n): 

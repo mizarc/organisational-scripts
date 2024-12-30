1599e5e1078d47a1c52c25eb1df3a1197de999ce
Mizarc <contact@mizarc.dev>
2024-12-30 21:27:43 +1000

    Add exception handling to transfer scripts
    This importantly highlights whether the file was successfully
    transferred, which can prove useful in determining whether to clear the
    input or not.

--- screen recs/Transfer.ps1
+++ screen recs/Transfer.ps1
@@ -4,35 +4,40 @@ param (
 )
 
 if (!$InputFolder) {
-  $InputFolder = Read-Host "Please enter the input folder path"
+    $InputFolder = Read-Host "Please enter the input folder path"
 }
 
 if (!$OutputFolder) {
-  $OutputFolder = Read-Host "Please enter the output folder path"
+    $OutputFolder = Read-Host "Please enter the output folder path"
 }
-
 
 # Get all video files in the input folder
 $imageFiles = Get-ChildItem -Path $inputFolder
 
-# Loop through each video file and crop them
+# Loop through each file and transfer them
 foreach ($file in $imageFiles) {
     if ($file.Name -match '\d{4}') {
-        # Define destination folder path using the extracted year
         $year = $matches[0]
-        $outputFolderPath = Join-Path -Path $outputFolder -ChildPath $year
-        
-        # Create the destination folder if it doesn't exist 
-        if (-not (Test-Path -Path $outputFolderPath)) { 
-            New-Item -Path $outputFolderPath -ItemType Directory | Out-Null 
+
+        try {
+            # Define destination folder path using the extracted year
+            $outputFolderPath = Join-Path -Path $outputFolder -ChildPath $year
+            
+            # Create the destination folder if it doesn't exist 
+            if (-not (Test-Path -Path $outputFolderPath)) { 
+                New-Item -Path $outputFolderPath -ItemType Directory | Out-Null 
+            }
+
+            # Define the destination path for the file
+            $outputPath = Join-Path -Path $outputFolderPath -ChildPath $file.Name
+
+            # Move the file to the destination folder
+            Move-Item -Path $file.FullName -Destination $outputPath
+        } catch {
+            Write-Host "Failed to move to $outputFolder, drive does not exist." -ForegroundColor Magenta
+            return
         }
-
-        # Define the destination path for the file
-        $outputPath = Join-Path -Path $outputFolderPath -ChildPath $file.Name
-
-        # Move the file to the destination folder
-        Move-Item -Path $file.FullName -Destination $outputPath
     }
 }
 
-Write-Host "Files have been moved to $outputFolder."
+Write-Host "Files have been moved to $outputFolder." -ForegroundColor Green

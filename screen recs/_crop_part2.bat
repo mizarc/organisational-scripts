89a9184792ec09f38e325473247a32e381fc2337
Mizarc <contact@mizarc.dev>
2024-12-30 20:19:06 +1000

    Add transfer script back to crop part 2 screen rec script

--- screen recs/_crop_part2.bat
+++ screen recs/_crop_part2.bat
@@ -4,8 +4,7 @@ powershell -NoProfile -ExecutionPolicy Bypass -File "RenameFromModifiedDate.ps1"
 powershell -NoProfile -ExecutionPolicy Bypass -File "Process.ps1" -InputFolder "Post Crop" -OutputFolder "Output"
 powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateExif.ps1" -folder "Output"
 powershell -NoProfile -ExecutionPolicy Bypass -File "GenerateMetadata.ps1" -folder "Output"
-
-
+powershell -NoProfile -ExecutionPolicy Bypass -File "Transfer.ps1" -inputFolder "Output" -outputFolder "X:\gaming-clips"
 
 echo.
 set /p input=Clean up? (y/n): 

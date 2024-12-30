03c091c10c8532e1b90bfda100868f7b09a90482
Mizarc <contact@mizarc.dev>
2024-12-30 20:21:55 +1000

    Fix generate metadata for screen rec including timezone

--- screen recs/GenerateMetadata.ps1
+++ screen recs/GenerateMetadata.ps1
@@ -33,6 +33,7 @@ foreach ($file in $videoFiles) {
     # Replace the game name in the content with the game name in the file name
     $fileNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
     $gameName = ($fileNameWithoutExtension -replace 'Screen Rec \d{4}-\d{2}-\d{2} \d{2}-\d{2}-\d{2} ', '').ToLower()
+    $gameName = ($gameName -replace '\+\d{4} ', '').ToLower()
     $content = $contentTemplate -replace '{GameName}', $gameName
 
     # Define the new file name and path

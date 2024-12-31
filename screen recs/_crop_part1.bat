29535f27c48ba175af932d8b7330d564f3137d6a
Mizarc <contact@mizarc.dev>
2024-12-31 15:57:29 +1000

    Add error handling to screen rec run scripts

--- screen recs/_crop_part1.bat
+++ screen recs/_crop_part1.bat
@@ -1,4 +1,14 @@
 @echo off
 powershell -NoProfile -ExecutionPolicy Bypass -File "Rename.ps1" -folder "Pre Crop" -fallbackTimeZone "+1000"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
 powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateModifiedDate.ps1" -folder "Pre Crop"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
 pause
--- screen recs/_crop_part2.bat
+++ screen recs/_crop_part2.bat
@@ -1,12 +1,39 @@
 @echo off
 mkdir Output
-powershell -NoProfile -ExecutionPolicy Bypass -File "RenameFromModifiedDate.ps1" -folder "Post Crop"
-powershell -NoProfile -ExecutionPolicy Bypass -File "Process.ps1" -InputFolder "Post Crop" -OutputFolder "Output"
-powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateExif.ps1" -folder "Output"
-powershell -NoProfile -ExecutionPolicy Bypass -File "GenerateMetadata.ps1" -folder "Output"
-powershell -NoProfile -ExecutionPolicy Bypass -File "Transfer.ps1" -inputFolder "Output" -outputFolder "X:\gaming-clips"
+if %ERRORLEVEL% == 1 (
+    echo.
+)
 
-echo.
+powershell -NoProfile -ExecutionPolicy Bypass -File "RenameFromModifiedDate.ps1" -folder "Post Crop"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "Process.ps1" -InputFolder "Post Crop" -OutputFolder "Output"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "UpdateExif.ps1" -folder "Output"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "GenerateMetadata.ps1" -folder "Output"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "Transfer.ps1" -inputFolder "Output" -outputFolder "X:\gaming-clips"
+if %ERRORLEVEL% == 1 (
+    pause
+    exit /b 1
+)
+
 set /p input=Clean up? (y/n): 
 if /i "%input%"=="y" (
     powershell -NoProfile -ExecutionPolicy Bypass -File "Clear.ps1" -folder "Post Crop"
